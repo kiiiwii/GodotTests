c72b1274ce964fe8929329adef8add37713bd3bc
kiiiwii <stachurap24@gmail.com>
2020-01-23 20:17:18 +0100

    merge Plane into PlaneTests

--- IntegrationTests/PlaneTests.cs
+++ IntegrationTests/PlaneTests.cs
@@ -31,5 +31,80 @@ namespace IntegrationTests
 
             Assert.NotInRange(distance, 0 - _epsilon, 0 + _epsilon);
         }
+
+        [Fact]
+        public void PlaneTestHasPoint1()
+        {
+            var plane = new Plane(1, 0, 0, 1);
+            for (int j = 0; j < 50; j++)
+            {
+                Vector3 v3 = plane.GetAnyPoint();
+                Assert.True(plane.HasPoint(v3));
+            }
+        }
+        [Fact]
+        public void PlaneTestHasPoint2()
+        {
+            var plane = new Plane(0, 1, 0, (float)0.5);
+            for (int j = 0; j < 50; j++)
+            {
+                Vector3 v3 = plane.GetAnyPoint();
+                Assert.True(plane.HasPoint(v3));
+            }
+        }
+        [Fact]
+        public void PlaneTestHasPoint3()
+        {
+            var plane = new Plane(0, 0, 1, 0);
+            for (int j = 0; j < 50; j++)
+            {
+                Vector3 v3 = plane.GetAnyPoint();
+                Assert.True(plane.HasPoint(v3));
+            }
+        }
+        [Fact]
+        public void IsPointOverTest1()
+        {
+            var plane = new Plane((float)1, 0, 0, 0);
+            Vector3 v3 = new Vector3((float)0.5, 0, 0);
+            Assert.True(plane.IsPointOver(v3));
+            plane = -plane;
+            Assert.False(plane.IsPointOver(v3));
+        }
+
+        [Fact]
+        public void IsPointOverTest2()
+        {
+            var plane = new Plane((float)1, 0, 0, 0);
+            Vector3 v3 = new Vector3(0, (float)1, 0);
+            Assert.True(plane.HasPoint(v3));
+            Assert.False(plane.IsPointOver(v3));
+            plane = -plane;
+            Assert.False(plane.IsPointOver(v3));
+        }
+
+        [Fact]
+        public void IsPointOverTest3()
+        {
+            var plane = new Plane(0, (float)1, 0, 0);
+            Vector3 v3 = new Vector3(0, (float)1, 0);
+            Assert.False(plane.HasPoint(v3));
+            Assert.True(plane.IsPointOver(v3));
+            plane = -plane;
+            Assert.False(plane.IsPointOver(v3));
+        }
+        [Fact]
+        public void IsPointOverTest4()
+        {
+            Vector3 v3Normal = new Vector3(1, 0, 0);
+            v3Normal = v3Normal.Normalized();
+            var plane = new Plane(v3Normal, 0);
+            Vector3 v3 = new Vector3(1, 0, 0);
+            Assert.True(plane.IsPointOver(v3));
+            plane = -plane;
+            Assert.False(plane.IsPointOver(v3));
+            v3 = v3.Reflect(new Vector3(0, 1, 1).Normalized());
+            Assert.True(plane.IsPointOver(v3));
+        }
     }
 }
